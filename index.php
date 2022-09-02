b67a21e3d4b7b5f34d11befd1581d986ccc1b6f8
Griminov Alexander <griminov@gmail.com>
2022-09-02 21:44:50 +0300

    directory

--- index.php
+++ index.php
@@ -26,8 +26,8 @@ $results = $Paginator->getData( $limit, $page);
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Гостевая книга</title>
 <!--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">-->
-    <link rel="stylesheet" href = "style.css">
-    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href = "style/style.css">
+    <link rel="stylesheet" href="style/bootstrap.min.css">
 </head>
 <body>
     <form action="" method="post">
